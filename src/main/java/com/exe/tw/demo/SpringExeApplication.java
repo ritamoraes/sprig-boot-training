ce21f68c38e58efe0aa7ee0e0f0f4e81add239cb
Rmoraes <ritac.moraes@gmail.com>
2018-10-11 18:15:10 -0300

    Projeto inicial

---
+++ src/main/java/com/exe/tw/demo/SpringExeApplication.java
@@ -0,0 +1,12 @@
+package com.exe.tw.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExeApplication.class, args);
+    }
+}
---
+++ src/main/java/com/training/tw/exe/ExeApplication.java
@@ -0,0 +1,12 @@
+package com.training.tw.exe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExeApplication.class, args);
+    }
+}
